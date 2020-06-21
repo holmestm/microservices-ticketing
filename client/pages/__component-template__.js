e74b39d58082390f05c41eb4e11ae29870bb23e6
Tim Holmes <tim@gravitaz.co.uk>
2020-06-21 20:31:11 +0100

    update every service

--- client/pages/__component-template__.js
+++
@@ -1,8 +0,0 @@
-const Component = () => {
-  return <div>Component</div>;
-};
-
-Component.getInitialProps = async (context, client, currentUser) => {
-  const { ticketId } = context.query;
-};
-export default Component;
