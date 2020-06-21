0f9fa7e2f1ba2622a77d7476515911079b5c796c
Tim Holmes <tim@gravitaz.co.uk>
2020-06-21 19:54:44 +0100

    fix bug with jwt and session

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -41,9 +41,7 @@ router.post(
       process.env.JWT_KEY!
     );
 
-    req.session = {
-      jwt: userJwt,
-    };
+    req.session!.jwt = userJwt;
 
     res.status(200).send(existingUser);
   }
