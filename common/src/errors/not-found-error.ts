d989841d17a3bac2d44aa4a0e083a897469be065
Tim Holmes <tim@gravitaz.co.uk>
2020-07-04 08:52:11 +0100

    Updates

--- common/src/errors/not-found-error.ts
+++ common/src/errors/not-found-error.ts
@@ -3,13 +3,13 @@ import { CustomError } from './custom-error';
 export class NotFoundError extends CustomError {
   statusCode = 404;
 
-  constructor() {
-    super('Route not found');
+  constructor(msg?: string) {
+    super(msg || 'Not found');
 
     Object.setPrototypeOf(this, NotFoundError.prototype);
   }
 
   serializeErrors() {
-    return [{ message: 'Not Found' }];
+    return [{ message: this.message || 'Not Found' }];
   }
 }
